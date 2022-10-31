b4118c9ccaf98b5362904c0118c9ddc18b76fd95
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-10-31 12:52:47 +0200

    Project12
    Project12

---
+++ src/A.java
@@ -0,0 +1,6 @@
+public class A implements  X{
+    @Override
+    public void Hellow() {
+        System.out.println("Hellow Dima");
+    }
+}
---
+++ src/Caller.java
@@ -0,0 +1,11 @@
+public class Caller {
+    private X x;
+
+    public Caller(X x) {
+        this.x = x;
+    }
+    public void call()
+    {
+        this.x.Hellow();
+    }
+}
